95945ffc0d322ce4cc1f5b9cb678875a920240dd
vortechs <jobartbenjamin@gmail.com>
2026-02-02 20:34:05 -0800

    added pathfind to pose code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -107,7 +107,6 @@ public class Robot extends LoggedRobot {
     // Threads.setCurrentThreadPriority(false, 10);
 
     field2d.setRobotPose(robotContainer.getDrive().getPose());
-
   }
 
   @Override
