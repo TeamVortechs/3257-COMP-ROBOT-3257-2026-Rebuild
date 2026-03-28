5f5547effe3c89000b1c59552257b246b7633e0b
AV-01 <aryavratmishra007@gmail.com>
2026-03-28 09:28:48 -0700

    Added Commands for VortechsController
    Enhance VortechsController rumble API: track rumble value via an overridden setRumble, make setRumbleCommand accept an amplitude, and add setRumbleRunCommand that uses RunCommand to apply rumble while active and reset it to 0 on finish. Includes necessary import and minor formatting cleanup for command-based rumble control.

--- src/main/java/frc/robot/util/VortechsController.java
+++ src/main/java/frc/robot/util/VortechsController.java
@@ -3,33 +3,38 @@ package frc.robot.util;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class VortechsController extends CommandXboxController {
 
+  private double rumbleValue;
 
-    private double rumbleValue;
+  public VortechsController(int port) {
+    super(port);
+    rumbleValue = 0;
+  }
 
-    public VortechsController(int port) {
-        super(port);
-        rumbleValue = 0;
-    }
-    
-    @Override
-    public void setRumble(GenericHID.RumbleType type, double value){
-        super.setRumble(type, value);
-        rumbleValue = value;
-    }
+  @Override
+  public void setRumble(GenericHID.RumbleType type, double value) {
+    super.setRumble(type, value);
+    rumbleValue = value;
+  }
 
-    public boolean isRumbling(){
-        return rumbleValue > 0;
-    }
+  public boolean isRumbling() {
+    return rumbleValue > 0;
+  }
 
-    public double getRumbleValue(){
-        return rumbleValue;
-    }
-    
-    public Command setRumbleCommand(){
-        return new InstantCommand(() -> this.setRumble(GenericHID.RumbleType.kBothRumble, 1.0));        
-    }
+  public double getRumbleValue() {
+    return rumbleValue;
+  }
+
+  public Command setRumbleCommand(double amplitude) {
+    return new InstantCommand(() -> this.setRumble(GenericHID.RumbleType.kBothRumble, amplitude));
+  }
+
+  public Command setRumbleRunCommand(double amplitude) {
+    return new RunCommand(() -> this.setRumble(GenericHID.RumbleType.kBothRumble, amplitude))
+        .finallyDo(() -> this.setRumble(GenericHID.RumbleType.kBothRumble, 0));
+  }
 }
